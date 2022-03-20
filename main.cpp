18649f4c99bcc6b5f2219f4211135303e0df1b82
MyeongJun Kim <kmj0568@gmail.com>
2022-03-20 19:07:08 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+void input()
+{
+
+}
+
+int DFS(int x, int y)
+{
+
+    return 0;
+}
+
+void solution()
+{
+
+}
+
+int main()
+{
+    input();
+    solution();
+    return 0;
+}
