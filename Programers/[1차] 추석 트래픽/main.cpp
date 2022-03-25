b9f9f263f359e5a770485c769aed65db47220f1e
MyeongJun Kim <kmj0568@gmail.com>
2022-03-25 23:37:53 +0900

    unsolved

---
+++ Programers/[1차] 추석 트래픽/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+int solution(vector<string> lines) {
+    int answer = 0;
+
+
+    return answer;
+}
+
+int main(void)
+{
+    FAST
+    vector<string> lines = {"2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"};
+    cout << solution(lines);
+
+    return 0;
+}
