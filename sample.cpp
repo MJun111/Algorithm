bc6e98aa4a1e589f832d4af9620f83ce14653274
MJun111 <100829030+MJun111@users.noreply.github.com>
2022-04-13 17:09:51 +0900

    Create sample.cpp
    sample

---
+++ sample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+void input()
+{
+
+}
+
+void solution()
+{
+
+}
+
+int main()
+{
+    FAST
+    input();
+    solution();
+
+    return 0;
+}
