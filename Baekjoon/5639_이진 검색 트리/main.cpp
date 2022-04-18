1e000d8a8674e90dbf3b3bd733aceb791299b00c
MyeongJun Kim <kmj0568@gmail.com>
2022-04-18 11:44:24 +0900

    5639_solved

---
+++ Baekjoon/5639_이진 검색 트리/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 10000 + 1
+
+int tree[MAX];
+int n = 0;
+
+void input()
+{
+	int num;
+	while (cin >> num)
+		tree[n++] = num;
+}
+
+void postorder(int start, int end)
+{
+	if (start >= end)
+		return;
+
+	int i;
+	for (i = start + 1; i < end; i++)
+		if (tree[start] < tree[i])
+			break;
+
+	postorder(start + 1, i);			// ¿ŞÂÊ ¼­ºêÆ®¸® Å½»ö
+	postorder(i, end);					// ¿À¸¥ÂÊ ¼­ºêÆ®¸® Å½»ö
+	cout << tree[start] << "\n";
+}
+
+void solution()
+{
+	postorder(0, n);
+}
+
+int main()
+{
+	FAST
+	input();
+	solution();
+
+	return 0;
+}
