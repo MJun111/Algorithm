c146cb68138836fc0f764f3ac518104da114ec7d
MyeongJun Kim <kmj0568@gmail.com>
2022-04-25 15:31:46 +0900

    11729_ing

---
+++ Baekjoon/11729_하노이 탑 이동 순서/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 20 + 1
+
+int k;
+
+void input()
+{
+    cin >> k;
+}
+
+void solution()
+{
+
+}
+
+int main()
+{
+    FAST
+    input();
+    solution();
+
+    return 0;
+}
