e0639ad691c2b34382fedbf70966eec99c799842
MyeongJun Kim <kmj0568@gmail.com>
2022-04-26 17:07:36 +0900

    11729_solved

--- Baekjoon/11729_하노이 탑 이동 순서/main.cpp
+++ Baekjoon/11729_하노이 탑 이동 순서/main.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
-#include <stack>
+#include <cmath>
 using namespace std;
 #define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 #define MAX 20 + 1
 
 int k;
-stack<int> st[3];
 
 void input()
 {
     cin >> k;
 }
 
+void hannoi(int start, int mid, int end, int n)
+{
+    if (n == 1)
+        cout << start << " " << end << "\n";
+    else
+    {
+        hannoi(start, end, mid, n - 1);
+        cout << start << " " << end << "\n";
+        hannoi(mid, start, end, n - 1);
+    }
+}
+
 void solution()
 {
-    for (int i = k; i > 0; i--)
-        st[0].push(i);
-
-    while (k > 0)
-    {
-        if (k % 2 == 1)
-        {
-            st[2].push(st[0].top());
-            st[0].pop();
-
-            
-            k--;
-        }
-        else
-        {
-            st[1].push(st[0].top());
-            st[0].pop();
-
-            while (st[0].empty())
-            {
-
-            }
-            k--;
-        }
-    }
+    cout << (int)pow(2, k) - 1 << "\n";
+    hannoi(1, 2, 3, k);
 }
 
 int main()
