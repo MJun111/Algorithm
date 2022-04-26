fed67795654d052dcc062882848a968171d94489
MyeongJun Kim <kmj0568@gmail.com>
2022-04-26 09:55:47 +0900

    11729_ing

--- Baekjoon/11729_하노이 탑 이동 순서/main.cpp
+++ Baekjoon/11729_하노이 탑 이동 순서/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 #define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 #define MAX 20 + 1
 
 int k;
+stack<int> st[3];
 
 void input()
 {
@@ -12,7 +14,31 @@ void input()
 
 void solution()
 {
+    for (int i = k; i > 0; i--)
+        st[0].push(i);
 
+    while (k > 0)
+    {
+        if (k % 2 == 1)
+        {
+            st[2].push(st[0].top());
+            st[0].pop();
+
+            
+            k--;
+        }
+        else
+        {
+            st[1].push(st[0].top());
+            st[0].pop();
+
+            while (st[0].empty())
+            {
+
+            }
+            k--;
+        }
+    }
 }
 
 int main()
