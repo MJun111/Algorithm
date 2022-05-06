d062cac6a198d3f236afb84bf7066bbee535dd80
MyeongJun Kim <kmj0568@gmail.com>
2022-05-06 13:53:06 +0900

    unsolved

---
+++ Baekjoon/1562_계단 수/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MOD 1000000000
+#define MAX 100 + 1
+
+int n;
+long long dp[MAX];
+
+void input()
+{
+    cin >> n;
+}
+
+void solution()
+{
+    dp[10] = 1;
+
+}
+
+int main()
+{
+    FAST
+    input();
+    solution();
+   
+    return 0;
+}
