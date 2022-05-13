3c21b00d2d67f91e131470e812b9f11b15bc232f
MyeongJun Kim <kmj0568@gmail.com>
2022-05-14 01:41:41 +0900

    2176_solved

--- Baekjoon/2176_합리적인 이동경로/main.cpp
+++ Baekjoon/2176_합리적인 이동경로/main.cpp
@@ -1,15 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+#include <climits>
 using namespace std;
 #define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 1001
+
+int n, m;
+vector<pair<int, int>> edge[MAX];
+int dist[MAX];
+int dp[MAX];
 
 void input()
 {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge[a].push_back({ b, c });
+        edge[b].push_back({ a, c });
+    }
+}
 
+void dijkstra()
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({ 0, 2 });
+    dist[2] = 0;
+    dp[2] = 1;
+
+    while (!pq.empty())
+    {
+        auto cur = pq.top();
+        pq.pop();
+
+        if (cur.first > dist[cur.second])
+            continue;
+
+        for (auto x : edge[cur.second])
+        {
+            int nextDis = x.second + cur.first;
+
+            if (nextDis < dist[x.first])
+            {
+                dist[x.first] = nextDis;
+                pq.push({ nextDis, x.first });
+            }
+
+            if (cur.first > dist[x.first])
+                dp[cur.second] += dp[x.first];
+        }
+    }
 }
 
 void solution()
 {
+    fill(dist, dist + MAX, INT_MAX);
 
+    dijkstra();
+    cout << dp[1] << "\n";
 }
 
 int main()
