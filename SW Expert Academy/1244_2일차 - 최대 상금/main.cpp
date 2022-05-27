5e8464f5726a7d14866c26b8e7bf5decec826e26
MyeongJun Kim <kmj0568@gmail.com>
2022-05-27 23:49:08 +0900

    1244_ing

---
+++ SW Expert Academy/1244_2일차 - 최대 상금/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+int t;
+
+void print(int tc, string ans)
+{
+    cout << "#" << tc << " " << ans << "\n";
+}
+
+void solution()
+{
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+    {
+
+    }
+}
+
+int main()
+{
+    FAST
+    solution();
+
+    return 0;
+}
