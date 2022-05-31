f8042708e4e662c0cea44620f77229c4ee510dd3
MyeongJun Kim <kmj0568@gmail.com>
2022-05-31 18:57:40 +0900

    1525_solved

--- Baekjoon/1525_퍼즐/main.cpp
+++ Baekjoon/1525_퍼즐/main.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
+#include <string>
 #include <queue>
+#include <set>
+#include <algorithm>
 using namespace std;
 #define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
 
-int ans[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 0} };
-int map[3][3];
-pair<int, int> st;
-int dir[4][2] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-
-struct _move {
-    int r;
-    int c;
-    int cnt;
-};
+string str;
+string goal = "123456780";
+set<string> visited;
+int dir[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
 
 void input()
 {
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-        {
-            cin >> map[i][j];
-            if (map[i][j] == 0)
-                st.first = i, st.second = j;
-        }
+    for (int i = 0; i < 9; i++)
+    {
+        char c;
+        cin >> c;
+        str += c;
+    }
 }
 
-void BFS()
+int BFS()
 {
-    queue<_move> q;
-    q.push({ st.first, st.second, 0 });
+    queue<pair<string, int>> q;
+    q.push({ str, 0 });
 
     while (!q.empty())
     {
-        int r = q.front().r;
-        int c = q.front().c;
-        int moveCnt = q.front().cnt;
+        string cur = q.front().first;
+        int cnt = q.front().second;
         q.pop();
+        
+        if (cur == goal)
+            return cnt;
+
+        int zero = cur.find('0');
+        int r = zero / 3;
+        int c = zero % 3;
 
         for (int i = 0; i < 4; i++)
         {
@@ -43,16 +45,24 @@ void BFS()
             int nc = c + dir[i][1];
 
             if (nr < 0 || nc < 0 || nr >= 3 || nc >= 3) continue;
-
-
+            
+            string next = cur;
+            swap(next[3 * r + c], next[3 * nr + nc]);
+            if (visited.find(next) == visited.end())
+            {
+                visited.insert(next);
+                q.push({ next, cnt + 1 });
+            }
         }
-
     }
+    return -1;
 }
 
 void solution()
 {
-    BFS();
+    cout << BFS() << "\n";
+
+
 }
 
 int main()
