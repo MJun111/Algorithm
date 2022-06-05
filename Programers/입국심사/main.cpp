b08c637c5a14544a7b7682e7b0d462fc1b5639e4
MyeongJun Kim <kmj0568@gmail.com>
2022-06-05 15:12:29 +0900

    solved

---
+++ Programers/입국심사/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define FAST cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+
+long long solution(int n, vector<int> times) {
+    long long left = 0, right = 1000000000000000000;
+    while (left < right)
+    {
+        long long mid = (left + right) / 2;
+        long long cnt = 0;
+        for (auto t : times)
+            cnt += mid / t;
+
+        cout << mid << ", " << cnt << "\n";
+
+        if (cnt >= n)
+            right = mid;
+        else
+            left = mid + 1;
+    }
+    return left;
+}
+
+int main()
+{
+    FAST
+    int n = 3;
+    vector<int> times = { 1, 1, 1 };
+    cout << solution(n, times);
+
+    return 0;
+}
