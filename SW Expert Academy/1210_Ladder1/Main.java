ddbdcf4d2f8c1ab55b83fb24ac4fd9f3261debba
MJun111 <100829030+MJun111@users.noreply.github.com>
2022-08-02 22:24:53 +0900

    1210_solved

--- SW Expert Academy/1210_Ladder1/Main.java
+++ SW Expert Academy/1210_Ladder1/Main.java
@@ -5,64 +5,47 @@ import java.io.*;
 
 public class SWEA_1210_Ladder1 {
 	public static void main(String[] args) throws IOException {
+//		System.setIn(new FileInputStream("data/1210_input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int t = 10;
+		int size = 100;
 		
 		while(t-- > 0) {
 			int ans = -1;
 			int tc = Integer.parseInt(br.readLine());
-			int[] dx = {0, 1, -1};
-			int[] dy = {1, 0, 0};
-			int[][] map = new int[100][100];
+			int target = -1;
+			int[] dx = {0, -1, 1};
+			int[] dy = {-1, 0, 0};
+			int[][] map = new int[size][size];
 			
-			for (int i = 0; i < 100; i++) {
+			for (int i = 0; i < size; i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < 100; j++) {
+				for (int j = 0; j < size; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j] == 2)
+						target = j;
 				}
 			}
 			
-			// 시작 좌표를 담을 큐
-			Queue<Integer> q = new LinkedList<>();
-			for (int i = 0; i < 100; i++) 
-				if (map[0][i] == 1)
-					q.add(i);
-			
-			while(!q.isEmpty()) {
-				int startX = q.poll();
-				int x = startX;
-				int y = 0;
-				int dir;	
-				boolean[][] visited = new boolean[100][100];
+			int y = size - 1;
+			int x = target;
+			int dir = 0;
+			boolean[][] visited = new boolean[size][size];
+			while(y != 0) {
+				visited[y][x] = true;
+				if (x - 1 >= 0 && map[y][x - 1] == 1 && !visited[y][x - 1])
+					dir = 1;
+				else if (x + 1 < size && map[y][x + 1] == 1 && !visited[y][x + 1])
+					dir = 2;
+				else
+					dir = 0;
 				
-				while(true) {
-					visited[y][x] = true; 
-					if (x + 1 < 100 && map[y][x + 1] == 1 && !visited[y][x + 1])
-						dir = 1;
-					else if (x - 1 >= 0 && map[y][x - 1] == 1 && !visited[y][x - 1])
-						dir = 2;
-					else
-						dir = 0;
-					
-					switch(dir) {
-					case 0: y++; break;
-					case 1: x++; break;
-					case 2: x--; break;
-					}
-					
-					if (y == 99) {
-						if (x == 99)
-							ans = startX;
-						break;
-					}
-					
-				}
-				if (ans != -1)
-					break;
+				x += dx[dir];
+				y += dy[dir];
 			}
 			
-			System.out.println("#" + tc + " " + ans);
+			System.out.println("#" + tc + " " + x);
 		}
 		
 	}
