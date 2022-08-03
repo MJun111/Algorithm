9b288e66cb963fcd3915efaae2da437d73a9370e
MJun111 <100829030+MJun111@users.noreply.github.com>
2022-08-03 19:01:48 +0900

    Update_11660

--- Baekjoon/BOJ_11660/Main.java
+++ Baekjoon/BOJ_11660/Main.java
@@ -2,37 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		int[][] arr = new int[n + 1][n + 1];
-		
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= n; j++) {
-				arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
-			}
-		}
-			
-		for (int i = 1; i <= m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x1 = Integer.parseInt(st.nextToken());
-			int y1 = Integer.parseInt(st.nextToken());
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			
-			int sum = 0;
-			for (int r = y1; r <= y2; r++) {
-				sum += arr[r][x2] - arr[r][x1 - 1];
-			}
-			sb.append(sum).append("\n");
-		}
-		System.out.println(sb.toString());
-	}
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = arr[i][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+            for (int r = y1; r <= y2; r++) {
+                sum += arr[r][x2] - arr[r][x1 - 1];
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
 }
