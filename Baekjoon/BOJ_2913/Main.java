1b9cd745983c0f78a979d0e6a47ddb1c995f132f
MyeongJun Kim <100829030+MJun111@users.noreply.github.com>
2022-10-12 17:54:56 +0900

    Solve_BOJ_2913

--- Baekjoon/BOJ_2913/Main.java
+++ Baekjoon/BOJ_2913/Main.java
@@ -1,8 +1,10 @@
+package com.ssafy.day1012.problem;
+
 import java.io.*;
 import java.util.*;
 
 // 출력 행 열 + 1 씩
-public class Main {
+public class BOJ_2931 {
     static class Point {
         int r, c, dir;
 
@@ -17,13 +19,14 @@ public class Main {
             this.dir = dir;
         }
     }
-    static int r, c;   // dir : 0, 1, 2, 3 -> 동, 서, 남, 북
+    static int r, c, er, ec, epipe, cnt1, cnt2;   // dir : 0, 1, 2, 3 -> 동, 서, 남, 북
     static char[][] map;
     static Point M, Z;
     static int[] dr = {0, 0, 1, -1};
     static int[] dc = {1, -1, 0, 0};
     static boolean flag;
-
+    static char[] pipes = {'|', '1', '2', '3', '4', '-', '+'};
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -35,6 +38,8 @@ public class Main {
         for (int i = 0; i < r; i++) {
             map[i] = br.readLine().toCharArray();
             for (int j = 0; j < c; j++) {
+            	if (map[i][j] != '.') cnt1++;
+            	if (map[i][j] == '+') cnt1++;
                 if (map[i][j] == 'M') {
                     M = new Point(i, j);
                 }
@@ -43,7 +48,7 @@ public class Main {
                 }
             }
         }
-
+        
         findSP();
 
     }
@@ -53,42 +58,79 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int nr = M.r + dr[i];
             int nc = M.c + dc[i];
-
+            
+            if (nr < 0 || nc < 0 || nr >= r || nc >= c) continue;
             // 파이프가 있다면
             if (map[nr][nc] != '.') {
                 int dir = checkDir(i, map[nr][nc]);
                 if (dir == -1) continue;
-                bfs(new Point(nr, nc, dir));
-                return;
+                bfs(new Point(nr, nc, i));
             }
         }
-
-        // 사방에 파이프가 없다면
-
 
     }
 
     static void bfs(Point sp) {
         Queue<Point> q = new ArrayDeque<>();
-        boolean[][][] visited = new boolean[4][r][c];   // 방향, 좌표
-        visited[sp.dir][sp.r][sp.c] = true;
         q.add(sp);
 
         while (!q.isEmpty()) {
+        	cnt2++;
             Point cur = q.poll();
             int dir = cur.dir;
-            int nr = cur.r + dr[dir];
-            int nc = cur.c + dc[dir];
-
+            
             int nextDir = checkDir(dir, map[cur.r][cur.c]);
-
+            if (nextDir == -1) return;
+            
+            int nr = cur.r + dr[nextDir];
+            int nc = cur.c + dc[nextDir];
+        	
+            if (nr < 0 || nc < 0 || nr >= r || nc >= c) return;
+            
+            // 가스가 갈 수 없음
+            if (map[nr][nc] == '.') {
+            	er = nr + 1;
+            	ec = nc + 1;
+            	for (int i = 0; i < 7; i++) {
+            		map[nr][nc] = pipes[i];
+            		epipe = i;
+            		
+            		if (pipes[i] == '+')
+            			dfs(nr, nc, nextDir, cnt2);
+            		else
+            			dfs(nr, nc, nextDir, cnt2 + 1);
+            		map[nr][nc] = '.';
+            	}
+            }
+            
+            q.add(new Point(nr, nc, nextDir));
         }
 
     }
+    
+    static void dfs(int y, int x, int dir, int cnt) {
+    	if (map[y][x] == 'Z') {
+			if (cnt1 == cnt) {
+				System.out.println(er + " " + ec + " " + pipes[epipe]);
+				System.exit(0);
+			}
+    		
+    		return;
+    	}
+    	int nextDir = checkDir(dir, map[y][x]);
+    	if (nextDir == -1) return;
 
+    	int nr = y + dr[nextDir];
+    	int nc = x + dc[nextDir];
+
+    	if (nr < 0 || nc < 0 || nr >= r || nc >= c) return;
+    	
+    	dfs(nr, nc, nextDir, cnt + 1);
+    }
+    
     // dir : 0, 1, 2, 3 -> 동, 서, 남, 북
-    static int checkDir(int dir, char c) {
-        switch(c) {
+    static int checkDir(int dir, char pipe) {
+        switch(pipe) {
             case '|': if (dir == 2 || dir == 3) return dir; break;
             case '-': if (dir == 0 || dir == 1) return dir; break;
             case '+': return dir;
