f17aaac31819f99b3164bbfbccd2e47028eb0218
MyeongJun Kim <100829030+MJun111@users.noreply.github.com>
2022-10-14 12:57:58 +0900

    Update Main.java

--- SW Expert Academy/2112_보호필름/Main.java
+++ SW Expert Academy/2112_보호필름/Main.java
@@ -1,11 +1,10 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Solution {
-	static int[][] film;
 	static int d, w, k, ans;
-	static boolean[] visited;
+	static int[][] film, copy;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -14,100 +13,88 @@ public class Solution {
 		int t = Integer.parseInt(br.readLine());
 		
 		for (int tc = 1; tc <= t; tc++) {
+			sb.append("#" + tc + " ");
 			st = new StringTokenizer(br.readLine());
-			
 			d = Integer.parseInt(st.nextToken());
 			w = Integer.parseInt(st.nextToken());
 			k = Integer.parseInt(st.nextToken());
-			ans = k;
-			
+			ans = Integer.MAX_VALUE;
 			film = new int[d][w];
+			copy = new int[d][w];			
 			
 			for (int i = 0; i < d; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < w; j++) {
-					film[i][j] = Integer.parseInt(st.nextToken());
+					film[i][j] = copy[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			// 0번 시행
-			if (k == 1 || check(film)) {
-				sb.append("#" + tc + " 0\n");
+			if (k == 1 || check()) {
+				sb.append("0\n");
 				continue;
 			}
 			
-			solve(0, 0, film);
-			
-			sb.append("#" + tc + " " + ans + "\n");
-			
+			simulation(0, 0);
+			sb.append(ans + "\n");
 		}
 		System.out.print(sb);
 	}
 	
-	static void solve(int cnt, int row, int[][] arr) {
-		if (cnt > ans)
-			return;
+	static void simulation(int row, int cnt) {
+		if (cnt > ans) return;
 		
 		if (row == d) {
-			if (check(arr)) {
-				ans = Math.min(ans, cnt);
-			}
+			if (check())
+				ans = Math.min(cnt, ans);
 			return;
 		}
 		
-		int[][] cArr = copyArr(arr);
+		// 약물 주입 X
+		simulation(row + 1, cnt);
 		
+		// 약물 A 주입
+		injection(row, 0);
+		simulation(row + 1, cnt + 1);
 		
-		solve(cnt, row + 1, cArr);
+		// 약물 B 주입
+		injection(row, 1);
+		simulation(row + 1, cnt + 1);
 		
-		cArr[row] = injection(cArr[row], 0);
-		solve(cnt + 1, row + 1, cArr);
-		
-		cArr[row] = injection(cArr[row], 1);
-		solve(cnt + 1, row + 1, cArr);
-		
-		
-	}
-
-	static boolean check(int[][] arr) {
-		boolean isSame;
 		for (int j = 0; j < w; j++) {
+			film[row][j] = copy[row][j];
+		}
+	}
+	
+	static boolean check() {
+		boolean flag;
+		for (int j = 0; j < w; j++) {
+			flag = false;
 			int cnt = 1;
-			int attr = arr[0][j];
-			isSame = false;
+			int attr = film[0][j];
 			
 			for (int i = 1; i < d; i++) {
-				if (arr[i][j] != attr) {
+				if (attr == film[i][j]) cnt++;
+				else {
 					cnt = 1;
-					attr = arr[i][j];
-				} else {
-					cnt++;
+					attr = film[i][j];
 				}
 				if (cnt == k) {
-					isSame = true;
+					flag = true;
 					break;
 				}
 			}
-			if (!isSame)
+			
+			if (!flag)
 				return false;
 		}
+		
 		return true;
 	}
 	
-	static int[][] copyArr(int[][] arr) {
-		int[][] cArr = new int[d][w];
-		
-		for (int i = 0; i < d; i++)
-			cArr[i] = arr[i].clone();
-		
-		return cArr;
-	}
-	
-	static int[] injection(int[] arr, int attr) {
-		for (int i = 0; i < w; i++) {
-			arr[i] = attr;
+	static void injection(int row, int attr) {
+		for (int j = 0; j < w; j++) {
+			film[row][j] = attr;
 		}
-		return arr;
 	}
 	
 }
