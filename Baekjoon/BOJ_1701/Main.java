3c0ac9b5e297d79e462821aedaa448d911b57e77
MyeongJun Kim <100829030+MJun111@users.noreply.github.com>
2022-10-23 03:07:59 +0900

    Solve_BOJ_1701

---
+++ Baekjoon/BOJ_1701/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        int n = input.length(), max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, getMax(input.substring(i, n)));
+        }
+        System.out.println(max);
+    }
+
+    static int getMax(String substr) {
+        int j = 0, n = substr.length(), max = 0;
+        int pi[] = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            while (j > 0 && substr.charAt(i) != substr.charAt(j)) j = pi[j-1];
+
+            if (substr.charAt(i) == substr.charAt(j)) {
+                max = Math.max(max, pi[i] = ++j);
+            }
+        }
+
+        return max;
+    }
+}
