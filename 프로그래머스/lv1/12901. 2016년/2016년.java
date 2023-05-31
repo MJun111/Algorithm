4f5140c93e854a386d18acc11e8d0fa80cd1cf64
MyeongJun Kim <100829030+MJun111@users.noreply.github.com>
2023-05-31 15:09:58 +0900

    [level 1] Title: 2016년, Time: 1.39 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -2,8 +2,6 @@ import java.time.*;
 
 class Solution {
     public String solution(int a, int b) {
-        LocalDate date = LocalDate.of(2016, a, b);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        return dayOfWeek.name().substring(0, 3);
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
     }
 }
