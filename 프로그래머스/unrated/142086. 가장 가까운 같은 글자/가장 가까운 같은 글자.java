573d44d0a0455d67a5b7b3d30649571f8ab4e76e
MyeongJun Kim <100829030+MJun111@users.noreply.github.com>
2023-05-31 11:27:11 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 0.72 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -3,17 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
+        int[] alpha = new int[26];
         
-        Map<Character, Integer> map = new HashMap<>();
+        Arrays.fill(alpha, -1);
         
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (!map.containsKey(c)) {
-                answer[i] = -1;
-            } else {
-                answer[i] = i - map.get(c);
-            }
-            map.put(c, i);
+            if (alpha[c - 'a'] == -1)
+                answer[i] = alpha[c - 'a'];
+            else
+                answer[i] = i - alpha[c - 'a'];
+                
+            alpha[c - 'a'] = i;
         }
         
         return answer;
