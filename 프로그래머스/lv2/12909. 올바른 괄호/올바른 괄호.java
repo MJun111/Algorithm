f68782bc3b6af30c71e19cb5325c04b6485bb8f4
MyeongJun Kim <100829030+MJun111@users.noreply.github.com>
2023-06-06 10:54:39 +0900

    [level 2] Title: 올바른 괄호, Time: 4.15 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') count++;
+			else count--;
+
+			if (count < 0) return false;
+		}
+
+		if (count == 0) return true;
+		else return false;
+    }
+}
